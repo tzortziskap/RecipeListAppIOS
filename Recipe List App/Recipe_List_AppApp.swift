12af55f884546d70437926efbff17c3cd0fad7cc
Tzortzis Kapellas <tzortziskap@hotmail.com>
2021-08-08 06:00:18 +0300

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Tzortzis Kapellas on 8/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
