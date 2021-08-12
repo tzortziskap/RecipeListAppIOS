2c633f852c797c8848aa6cd6422348e0dc5ee5c7
Tzortzis Kapellas <tzortziskap@hotmail.com>
2021-08-13 01:25:09 +0300

    finish recipe detail

--- Recipe List App/ViewModels/RecipeModel.swift
+++ Recipe List App/ViewModels/RecipeModel.swift
@@ -20,4 +20,73 @@ class RecipeModel: ObservableObject {
         //Another way
         self.recipes = DataService.getLocalData()
     }
+    
+    static func getPortion(ingredient:Ingredients, recipeServings:Int, targetServings:Int) -> String {
+        
+        var portion = ""
+        var numerator = ingredient.num ?? 1
+        var denominator = ingredient.denom ?? 1
+        var wholePortions = 0
+        
+        if ingredient.num != nil {
+            
+            //Get a single size by multiplying denominator by the recipe servings
+            denominator = denominator * recipeServings
+            
+            //Get target portion by multiplying numerator by target servings
+            numerator *= targetServings
+            
+            //Reduce fraction by greatest common divisor
+            let divisor = Rational.greatestCommonDivisor(numerator, denominator)
+            numerator /= divisor
+            denominator /= divisor
+            
+            //Get the whole portion if numerator > denominator
+            if numerator >= denominator {
+                
+                //Calculated whole portions
+                wholePortions = numerator / denominator
+                
+                //Canculate the remainder
+                numerator = numerator % denominator
+                
+                //Assign to portion string
+                portion += String (wholePortions)
+            }
+            
+            //Express the remainder as a fraction
+            if numerator > 0 {
+                
+                //Assign remainder as fraction to the portion string
+                portion += wholePortions > 0 ? " " : ""
+                portion += "\(numerator)/\(denominator)"
+            }
+        }
+        
+        if var unit = ingredient.unit {
+            
+            //If we need to pluralize
+            if wholePortions > 1 {
+                
+                //Calculate appropriate suffix
+                if unit.suffix(2) == "ch" {
+                    unit += "es"
+                }
+                else if unit.suffix(1) == "f" {
+                    unit = String(unit.dropLast())
+                    unit += "ves"
+                }
+                else {
+                    unit += "s"
+                }
+            }
+            
+            
+            portion += ingredient.num == nil && ingredient.denom == nil ? "" : " "
+            
+            return portion + unit
+        }
+        
+        return portion
+    }
 }
--- Recipe List App/Views/RecipeDetailView.swift
+++ Recipe List App/Views/RecipeDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct RecipeDetailView: View {
     
     var recipe : Recipe
+    @State var selectedServingSize = 2
     
     var body: some View {
         ScrollView {
@@ -21,6 +22,20 @@ struct RecipeDetailView: View {
                     .resizable()
                     .scaledToFill()
                 
+                //MARK: Serving Size Picker
+                VStack(alignment: .leading){
+                    Text("Select your serving size:")
+                    Picker("", selection: $selectedServingSize) {
+                        Text("2").tag(2)
+                        Text("4").tag(4)
+                        Text("6").tag(6)
+                        Text("8").tag(8)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .frame(width:160)
+                }
+                .padding()
+                
                 // Mark: Ingredients
                 VStack(alignment: .leading) {
                     Text("Ingredients")
@@ -28,7 +43,7 @@ struct RecipeDetailView: View {
                         .padding(.bottom, 5)
                     ForEach (recipe.ingredients){
                         item in
-                        Text("• " + item.name)
+                        Text("• " + RecipeModel.getPortion(ingredient:item, recipeServings: recipe.servings, targetServings: selectedServingSize) + " " + item.name.lowercased())
                     }
                 }
                 .padding(.horizontal)
